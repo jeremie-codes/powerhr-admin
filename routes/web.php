709a3a531960373a90539076a97771b8aba2eefd
jeremie <jeremie-codes>
2025-05-21 08:49:16 +0100

    dedes

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ Route::get('/access-denied', function () {
     return view('denied.show');
 })->name('access-denied');
 
-Route::domain('admin.localhost')->group(function () {
+Route::domain('admin.powerhr.site')->group(function () {
     Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified', 'admin'])->group(function () {
         Route::resource('customers', ClientController::class);
         Route::resource('users', UserController::class)->only([
@@ -62,7 +62,7 @@ Route::domain('admin.localhost')->group(function () {
     });
 });
 
-Route::domain('client.localhost')->group(function () {
+Route::domain('client.powerhr.site')->group(function () {
     Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified', 'client'])->group(function () {
     
         Route::post('account/register', [ClientUserController::class, 'create'])->name('account.register');
@@ -86,7 +86,7 @@ Route::domain('client.localhost')->group(function () {
     });
 });
 
-Route::domain('candidat.localhost')->group(function () {
+Route::domain('candidat.powerhr.site')->group(function () {
     Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified', 'candidate'])->group(function () {
     
         Route::get("/", [CandidateController::class, 'index'])->name('candidate.index');
