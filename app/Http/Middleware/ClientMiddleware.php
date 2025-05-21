dcb9fedbe3339d4eee142ba14ea54ab8e014af58
jeremie <jeremie-codes>
2025-05-21 09:00:11 +0100

    dedes

--- app/Http/Middleware/ClientMiddleware.php
+++ app/Http/Middleware/ClientMiddleware.php
@@ -18,7 +18,7 @@ class ClientMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (Auth::check() && Auth::user()->hasRole('client')) {
+        if (Auth::check() && Auth::user()->hasRole('employee')) {
             return $next($request);
         }
 
